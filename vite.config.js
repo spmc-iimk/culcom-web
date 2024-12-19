e5e43563fb9f846b299cc0ef6c27ee57fea451ec
manoranjan99 <155602009+manoranjaniitm@users.noreply.github.com>
2024-12-19 14:49:47 +0530

    added vite and made corresponding changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'src',
   server: {
     open: true, // Automatically open the browser on server start
   },
   build: {
-    outDir: 'dist',  // Output directory for the build
-  },
+    outDir: '../dist',  // Output directory for the build
+    rollupOptions: {
+      input: {
+        main: './src/home.html',
+        home: './src/home.html',
+        contactus: './src/contact-us.html',
+        team: './src/team.html'
+      }
+    }
+  }
 });
