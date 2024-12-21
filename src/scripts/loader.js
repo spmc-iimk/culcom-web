81e1576d59433b0a5fdaeb76d12cb694d76c9d2a
manoranjan99 <spmc@iimklive.com>
2024-12-22 01:24:42 +0530

    added smooth loading screen transitions

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -1,4 +1,15 @@
+import gsap from "gsap";
+
 window.onload = () => {
     const loadingScreen = document.getElementById("loading-screen");
-    loadingScreen.style.display = "none";
+
+    gsap.to(loadingScreen, {
+        opacity: 0,
+        scale: 0.9,
+        duration: 1,
+        ease: "power2.out",
+        onComplete: () => {
+          loadingScreen.style.display = "none";
+        },
+      });
 };
